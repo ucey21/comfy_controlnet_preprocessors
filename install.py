506d83d555ad7670a403c4e3564e8378a34550a6
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-03-03 18:50:09 +0700

    Add global shortcut module to fix install.py

--- install.py
+++ install.py
@@ -1,9 +1,16 @@
 import os
 from time import sleep
-from . import canny, hed, midas, mlsd, openpose, uniformer
-print("Installing requirements...")
-sleep(2)
-os.system("pip install -r requirements.txt")
+from importlib.util import spec_from_file_location, module_from_spec
+import sys
+
+module_name = "comfy_controlnet_preprocessors"
+EXT_PATH = os.path.dirname(os.path.realpath(__file__))
+def add_global_shortcut_module():
+    #Naming things is hard
+    module_spec = spec_from_file_location(module_name, os.path.join(EXT_PATH, "__init__.py"))
+    module = module_from_spec(module_spec)
+    sys.modules[module_name] = module
+    module_spec.loader.exec_module(module)
 
 def download_models():
     canny.CannyDetector()
@@ -12,6 +19,14 @@ def download_models():
     mlsd.MLSDdetector()
     openpose.OpenposeDetector()
     uniformer.UniformerDetector()
+
+print("Installing requirements...")
+sleep(2)
+os.system(f"pip install -r {EXT_PATH}/requirements.txt")
+
+add_global_shortcut_module()
+from comfy_controlnet_preprocessors import canny, hed, midas, mlsd, openpose, uniformer
+
 print("Download models...")
 sleep(2)
 download_models()
