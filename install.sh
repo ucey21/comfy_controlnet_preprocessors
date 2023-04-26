aeade7c8ac830c8d95c6ff2d8b6f693850ff80f9
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-04-26 20:44:53 +0700

    Fix install

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ -e "../../python_embeded/python" ]; then
+if [ -e "../../../python_embeded/python" ]; then
     echo Custom Python build of ComfyUI standalone executable detected:
     echo "$(readlink -f "../../python_embeded/python")"
     echo --------------------------------------------------
-    ../../python_embeded/python install.py ${1:+"$1"} #One-liner is za bezt
+    ../../../python_embeded/python install.py ${1:+"$1"} #One-liner is za bezt
 else
     echo "Custom Python not found. Use system's Python executable instead:"
     echo "$(which python)"
