4b7878a1f28044b0bca6ab7c65b8f5d050e40295
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-05-09 23:01:44 +0700

    .

--- v1/leres/__init__.py
+++ v1/leres/__init__.py
@@ -51,8 +51,9 @@ def apply_leres(input_image, thr_a, thr_b):
     if model is None:
         model_path = download_model_if_not_existed()
         checkpoint = torch.load(model_path, map_location='cpu')
-        model = RelDepthModel(backbone='resnext101').to(model_management.get_torch_device())
+        model = RelDepthModel(backbone='resnext101', map_location='cpu')
         model.load_state_dict(strip_prefix_if_present(checkpoint['depth_model'], "module."), strict=True)
+        model.to(model_management.get_torch_device())
         del checkpoint
 
     """ if boost and pix2pixmodel is None:
